ead0bf93c908e47799fe42185fc224a829520cdc
Parvez-Asghar, Tahira <tahira.parvez-asghar@accenture.com>
2020-01-21 10:56:52 +0100

    Added QuizEnd and costomized Quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
 
 let quizData = require('./quiz_data.json');
 
@@ -14,9 +15,17 @@ class Quiz extends Component {
     }
 
     render() {
+
+        const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
+
+        console.log(isQuizEnd);
+
         return (
             <div>
                 <div className="QuizQuestion">
+                    {
+                        isQuizEnd ? <QuizEnd /> : null
+                    }
                     <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
                 </div>
             </div>
---
+++ src/QuizEnd.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class QuizEnd extends Component {
+    render() {
+        return (
+            <div>
+                <p>THANKS</p>
+                <a href="">Reset</a>
+            </div>
+
+        );
+    }
+}
+
+export default QuizEnd;
