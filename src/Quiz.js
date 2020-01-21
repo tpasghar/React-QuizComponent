22ec2bc42bdf9b23055cdf490910921dd2d6371f
Parvez-Asghar, Tahira <tahira.parvez-asghar@accenture.com>
2020-01-21 09:43:32 +0100

    Added QuizQuestion and customized Quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import QuizQuestion from './QuizQuestion.js';
 
 let quizData = require('./quiz_data.json');
 
@@ -16,9 +17,7 @@ class Quiz extends Component {
         return (
             <div>
                 <div className="QuizQuestion">
-                    {
-                        quizData.quiz_questions[0].instruction_text
-                    }
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
                 </div>
             </div>
         );
